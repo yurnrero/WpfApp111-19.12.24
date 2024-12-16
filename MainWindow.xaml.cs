f7ba5fef3a9115cea861ec0e75bfd5c678bafae8
Admin <Admin@DESKTOP-7MCGF0R>
2024-12-16 16:30:22 +0700

    Добавьте файлы проекта.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,134 @@
+﻿using System.Collections.ObjectModel;
+using System.Linq;
+using System.Windows;
+
+namespace WpfApp1
+{
+    public partial class MainWindow : Window
+    {
+        // Коллекция товаров
+        private ObservableCollection<Product> Products { get; set; }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            
+            Products = new ObservableCollection<Product>
+            {
+                new Product { ProductName = "Ноутбук ASUS", Description = "Игровой ноутбук с RTX 3060", Manufacturer = "ASUS", Price = 129999, Stock = 15, ImagePath = "Images/laptop.png" },
+                new Product { ProductName = "Смартфон Samsung", Description = "Флагманский смартфон S23 Ultra", Manufacturer = "Samsung", Price = 99999, Stock = 7, ImagePath = "Images/phone.png" },
+                new Product { ProductName = "Телевизор LG", Description = "Крутой телевизор 4k", Manufacturer = "LG", Price = 99999, Stock = 2, ImagePath = "Images/tv.png" },
+                new Product { ProductName = "Наушники Sony", Description = "Беспроводные наушники с шумоподавлением", Manufacturer = "Sony", Price = 7999, Stock = 30, ImagePath = "Images/headphones.png" },
+                new Product { ProductName = "Мышь Logitech", Description = "Игровая мышь с подсветкой", Manufacturer = "Logitech", Price = 2499, Stock = 50, ImagePath = "Images/mouse.png" },
+                new Product { ProductName = "Клавиатура Corsair", Description = "Механическая клавиатура с RGB подсветкой", Manufacturer = "Corsair", Price = 7999, Stock = 20, ImagePath = "Images/keyboard.png" },
+                new Product { ProductName = "Планшет Apple", Description = "Планшет iPad Pro 12.9", Manufacturer = "Apple", Price = 84999, Stock = 12, ImagePath = "Images/ipad.png" }
+
+            };
+
+            
+            ProductList.ItemsSource = Products;
+
+            
+            UpdateProductCount();
+        }
+
+        
+        private void AddProduct_Click(object sender, RoutedEventArgs e)
+        {
+            
+            var newProduct = new Product
+            {
+                ProductName = "Новый товар",
+                Description = "Описание нового товара",
+                Manufacturer = "Производитель",
+                Price = 1000,
+                Stock = 10,
+                ImagePath = "Images/default.png"
+            };
+
+            
+            Products.Add(newProduct);
+
+            
+            ProductList.ItemsSource = Products;
+
+            
+            UpdateProductCount();
+        }
+
+        
+        private void UpdateProductCount()
+        {
+            ProductCount.Text = $"Показано товаров: {Products.Count}";
+        }
+
+        
+        private void SortOptions_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+        {
+            var selectedItem = SortOptions.SelectedItem as System.Windows.Controls.ComboBoxItem;
+
+            if (selectedItem != null && selectedItem.Content != null)
+            {
+                switch (selectedItem.Content.ToString())
+                {
+                    case "По цене":
+                        Products = new ObservableCollection<Product>(Products.OrderBy(p => p.Price));
+                        break;
+                    case "По имени":
+                        Products = new ObservableCollection<Product>(Products.OrderBy(p => p.ProductName));
+                        break;
+                    case "По наличию":
+                        Products = new ObservableCollection<Product>(Products.OrderBy(p => p.Stock));
+                        break;
+                    default:
+                        break;
+                }
+
+                
+                ProductList.ItemsSource = Products;
+                UpdateProductCount();
+            }
+        }
+
+        
+        private void ManufacturerFilter_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+        {
+            var selectedManufacturer = ManufacturerFilter.SelectedItem as System.Windows.Controls.ComboBoxItem;
+
+            if (selectedManufacturer != null && selectedManufacturer.Content != null)
+            {
+                
+                if (selectedManufacturer.Content.ToString() == "Все производители")
+                {
+                    ProductList.ItemsSource = Products; 
+                }
+                else
+                {
+                    
+                    var filteredProducts = Products.Where(p => p.Manufacturer == selectedManufacturer.Content.ToString()).ToList();
+                    ProductList.ItemsSource = new ObservableCollection<Product>(filteredProducts);
+                }
+
+                UpdateProductCount();
+            }
+        }
+
+       
+        private void Exit_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+    }
+
+    
+    public class Product
+    {
+        public string ProductName { get; set; }
+        public string Description { get; set; }
+        public string Manufacturer { get; set; }
+        public double Price { get; set; }
+        public int Stock { get; set; }
+        public string ImagePath { get; set; }
+    }
+}
---
+++ ProductView.xaml.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+   
+    public partial class ProductView : UserControl
+    {
+        public ProductView()
+        {
+            InitializeComponent();
+        }
+        public string ProductName
+        {
+            get { return (string)GetValue(ProductNameProperty); }
+            set { SetValue(ProductNameProperty, value); }
+        }
+        public static readonly DependencyProperty ProductNameProperty =
+            DependencyProperty.Register(nameof(ProductName), typeof(string), typeof(ProductView), new PropertyMetadata(string.Empty));
+
+       
+        public string Description
+        {
+            get { return (string)GetValue(DescriptionProperty); }
+            set { SetValue(DescriptionProperty, value); }
+        }
+        public static readonly DependencyProperty DescriptionProperty =
+            DependencyProperty.Register(nameof(Description), typeof(string), typeof(ProductView), new PropertyMetadata(string.Empty));
+
+        
+        public string Manufacturer
+        {
+            get { return (string)GetValue(ManufacturerProperty); }
+            set { SetValue(ManufacturerProperty, value); }
+        }
+        public static readonly DependencyProperty ManufacturerProperty =
+            DependencyProperty.Register(nameof(Manufacturer), typeof(string), typeof(ProductView), new PropertyMetadata(string.Empty));
+
+        
+        public decimal Price
+        {
+            get { return (decimal)GetValue(PriceProperty); }
+            set { SetValue(PriceProperty, value); }
+        }
+        public static readonly DependencyProperty PriceProperty =
+            DependencyProperty.Register(nameof(Price), typeof(decimal), typeof(ProductView), new PropertyMetadata(0m));
+
+       
+        public int Stock
+        {
+            get { return (int)GetValue(StockProperty); }
+            set { SetValue(StockProperty, value); }
+        }
+        public static readonly DependencyProperty StockProperty =
+            DependencyProperty.Register(nameof(Stock), typeof(int), typeof(ProductView), new PropertyMetadata(0));
+
+        
+        public string ImagePath
+        {
+            get { return (string)GetValue(ImagePathProperty); }
+            set { SetValue(ImagePathProperty, value); }
+        }
+        public static readonly DependencyProperty ImagePathProperty =
+            DependencyProperty.Register(nameof(ImagePath), typeof(string), typeof(ProductView), new PropertyMetadata(string.Empty));
+    }
+}
+
